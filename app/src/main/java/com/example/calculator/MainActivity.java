bbc0b88bfb593204076ea2b6d9e80a3313921114
Saptashrungi <saptashrungib@gmail.com>
2021-01-17 22:38:02 +0530

    added location

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -6,14 +6,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.content.Context;
 
-
+import android.util.Log;
 
 
 public class MainActivity extends AppCompatActivity {
    TextView num3;
    EditText num1;
     EditText num2;
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    protected Context context;
+    TextView txtLat;
+    String lat;
+    String provider;
+    protected String latitude,longitude;
+    protected boolean gps_enabled,network_enabled;
    Button add,sub,multiply,div,sin,cos,tan,pow,log,sqrt,clear,location;
     double number3,number1,number2;
     @Override
@@ -35,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         log=(Button)findViewById(R.id.log);
         sqrt=(Button)findViewById(R.id.sqrt);
         clear=(Button)findViewById(R.id.clear);
+        location=(Button)findViewById(R.id.location);
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -158,6 +171,37 @@ public class MainActivity extends AppCompatActivity {
                 num1.setText("");
                 num2.setText("");
                 num3.setText("");
+            }
+        });
+
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+                txtLat = (TextView) findViewById(R.id.textview1);
+                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            }
+            @Override
+            public void onLocationChanged(Location location) {
+                txtLat = (TextView) findViewById(R.id.textview1);
+                txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Log.d("Latitude","disable");
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                Log.d("Latitude","enable");
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                Log.d("Latitude","status");
             }
         });
 
